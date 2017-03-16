3a3b44661f46e0cd71b49191ec2fc5c0b87a24fe
witmicko <mogrodni@redhat.com>
2017-03-16 11:07:23 +0000

    does not work

--- src/app/main.js
+++ src/app/main.js
@@ -20,9 +20,38 @@ angular.module('app', [
   require('angular-material'),
   require('ng-sortable'),
   require('fh-wfm-mediator'),
-  // require('fh-wfm-workflow-angular')(adminConfig)
-  require('fh-wfm-workflow-angular')(userConfig)
+  require('fh-wfm-workflow-angular')(adminConfig)
+  // require('fh-wfm-workflow-angular')(userConfig)
 ]);
+
+// angular.module('app.admin', [
+//   require('angular-ui-router'),
+//   require('angular-material'),
+//   require('ng-sortable'),
+//   require('fh-wfm-mediator'),
+//   require('fh-wfm-workflow-angular')(adminConfig)
+// ]);
+
+angular.module('app')
+  .controller('AdminController', ['$scope', '$state', '$mdSidenav', 'mediator', function($scope, $state, $mdSidenav, mediator) {
+    mediatorSubscribers.unsubscribeAll(mediator);
+    mediatorSubscribers.setupSubscribers(mediator, $scope);
+
+    $scope.$state = $state;
+    $scope.toggleSidenav = function(event, menuId) {
+      $mdSidenav(menuId).toggle();
+      event.stopPropagation();
+    };
+    $scope.navigateTo = function(state, params) {
+      if (state) {
+        if ($mdSidenav('left').isOpen()) {
+          $mdSidenav('left').close();
+        }
+        $state.go(state, params);
+      }
+    };
+  }]);
+
 
 
 //Initialising the application with required serviceconfig and initialising script.
@@ -32,6 +61,13 @@ function AppConfig($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('app', {
       abstract: true,
+      templateUrl: 'app/main.tpl.html',
+      data: {
+        columns: 3
+      },
+      controller: 'AdminController'
+    })
+    .state('app.preview', {
       templateUrl: 'app/main.tpl.html',
       data: {
         columns: 3
